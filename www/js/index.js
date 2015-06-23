a0d3bc452d52831c677254c289618039eddd8b3d
jfactor <jfactor@compton.k12.ca.us>
2015-06-23 15:41:32 -0700

    Tue 06/23/2015

--- www/js/index.js
+++ www/js/index.js
@@ -29,6 +29,22 @@ function myApp() {
         _self.messagesObj.append(msg + '<br>');
     }
 
+    this.detectDeviceType = function () {
+        if (navigator.userAgent.match(/iPad/i) == "iPad")
+            return "ipad";
+        else
+            if (navigator.userAgent.match(/iPhone/i) == "iPhone")
+                return "iphone";
+            else
+                if (navigator.userAgent.match(/Android/i) == "Android")
+                    return "android";
+                else
+                    if (navigator.userAgent.match(/BlackBerry/i) == "BlackBerry")
+                        return "blackBerry";
+                    else
+                        return "undetected";
+    }
+
     this.onDeviceReady = function () {
         try
         {
@@ -37,7 +53,8 @@ function myApp() {
             StatusBar.backgroundColorByHexString("#aa0000");
 
             _self.postMessage('Device is Ready ...');
-        
+
+            _self.postMessage ('Device type is ' + _self.detectDeviceType());
             _self.initBatteryStatus();
             _self.initGeoLocation();
 
